bb4bb9fa5ad1050d7bf0c9f69d00f7c8daa82c30
gksubed <20123043s@gmail.com>
2026-08-18 13:06:47 +0800

    Just first cooment

--- routes/console.php
+++ routes/console.php
@@ -8,3 +8,5 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function (): void {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// hello raja
